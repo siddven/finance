77baccd5196d16bd15c6220e1b94c0274b50b947
siddven <siddven@users.noreply.github.com>
2021-01-05 23:28:50 +0000

    automated commit by submit50

--- population.c
+++ population.c
@@ -8,34 +8,36 @@ int main(void)
     // TODO: Prompt for start size
 
     do
-  {
-      start = get_int("Start size: ");
-  }
-  while (start < 9);
+    {
+        start = get_int("Start size: ");
+    }
+    while (start < 9);
 
 
 
     // TODO: Prompt for end size
-   do
-   {
-       end = get_int("End size:");
-   }
-   while (end < start);
+    do
+    {
+        end = get_int("End size:");
+    }
+    while (end < start);
 
     // TODO: Calculate number of years until we reach threshold
     int years = 0;
-    while (start < end) {
-    float born = start / 3;
-    float death = start/ 4;
-    start += born;
-    start -= death;
-    years += 1;}
-    printf("Years: %d",years);
-
+    while (start < end)
+    {
+        float born = start / 3;
+        float death = start / 4;
+        start += born;
+        start -= death;
+        years += 1;
     }
+    printf("Years: %d", years);
+
+}
 
 
 
 
 
-    // TODO: Print number of years
+// TODO: Print number of years
