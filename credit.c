8738181bad75a601a63b26e9b9076ca6120ca5e0
siddven <siddven@users.noreply.github.com>
2021-01-07 12:47:49 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -6,9 +6,9 @@ int main(void)
     long cardnumber;
     long a;
     long b;
-   long mult;
-   long mult2;
-   long mult3;
+   long mult=100;
+   long mult2=10;
+   long mult3=1;
    long add = 0;
    long addb = 0;
    long addall = 0;
@@ -18,11 +18,17 @@ int main(void)
     do {
 
          cardnumber = get_long("Enter a card number:");
-         mult = 100;
 
-         mult2 = 10;
-         mult3 = 1;
-         for (int i = 0; i < 16; ++i ) {
+
+
+         //printf("%li\n", b);
+
+         }
+
+
+    while (cardnumber < 1000000000000);
+
+    for (int i = 0; i < 16; ++i ) {
          a = cardnumber % mult;
          a = a / mult2;
          a = a * 2;
@@ -35,26 +41,22 @@ int main(void)
          b = cardnumber % mult2;
          b = b / mult3;
          addb += b;
-
-         ;
          mult = mult *100;
          mult2 = mult2 * 100;
          mult3 = mult3 * 100;
 
-         //printf("%li\n", b);
-
-         }
-
     }
-    while (cardnumber < 1000000000000);
+
          addall = addb + add;
 
          long an = addall % 10;
-         printf("%li\n", an);
+         //printf("%li\n", an);
+         if (an > 0) {
+             printf("INVALID\n");
+         }
 
 
-
-    if (cardnumber / 1000000000000 == 4 || cardnumber / 1000000000000000 == 4) {
+    else if (cardnumber / 1000000000000 == 4 || cardnumber / 1000000000000000 == 4) {
         if (an == 0) {
             printf("VISA\n");
 
@@ -63,6 +65,7 @@ int main(void)
     }
     else if (cardnumber / 100000000000000 == 55|| cardnumber / 100000000000000 == 54 || cardnumber / 100000000000000 == 53 || cardnumber / 100000000000000 == 52 || cardnumber / 100000000000000 == 51 ) {
         if (an ==0) {
+            
             printf("MASTERCARD\n");
 
         }
@@ -75,8 +78,9 @@ int main(void)
         }
 
     }
-    else if (an > 0) {
+    else {
         printf("INVALID\n");
     }
-}
+    }
+
 
