b6b26076ca9073bed7e01c46d8dc737136ce412e
siddven <siddven@users.noreply.github.com>
2021-02-15 15:29:26 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -132,18 +132,12 @@ void record_preferences(int ranks[])
                }
                for (int j = i; j < candidate_count-i; j ++)
        {
-          preferences[ranks[i]][j] += 1;
+          preferences[ranks[i]][j+1] += 1;
 
 
        }
            }
-   for (int i = 0; i < candidate_count; i++)
-   {
-       for (int j = 0; j < candidate_count; j++)
-       {
-           printf("%i\n", preferences[i][j]);
-       }
-   }
+
 
     return;
 }
