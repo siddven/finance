5ab09beb53b5d73df6c0eaaf2bd7ee8dbc1c32c7
siddven <siddven@users.noreply.github.com>
2021-02-15 18:54:01 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -150,13 +150,14 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
+    int a = 0;
     for (int i = 0; i < candidate_count; i++)
     {
 
 
         for (int j = 0; j < candidate_count; j++)
         {
-            int a = 0;
+
 
                 if (preferences[i][j] > preferences[j][i])
                 {
@@ -165,7 +166,7 @@ void add_pairs(void)
                     pair_count ++;
                     a++;
                 }
-                
+
         }
 
 
