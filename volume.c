6473f47229a0b017c0f36100f5fc3c9b8cc59f04
siddven <siddven@users.noreply.github.com>
2021-02-27 04:26:15 +0000

    automated commit by check50 [check50=True]

--- volume.c
+++ volume.c
@@ -35,7 +35,9 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    fwrite(*input, sizeof(uint8_t), 44, *output);
+    uint8_t buffer;
+    while (fread(&buffer, sizeof(uint8_t),1, input))
+    fwrite(&buffer, sizeof(uint8_t), 1, output);
 
 
     // TODO: Read samples from input file and write updated data to output file
