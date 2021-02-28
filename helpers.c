871ba16040e2a340c01667be2bbc4306f7ffdec9
siddven <siddven@users.noreply.github.com>
2021-02-28 18:09:24 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -3,19 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-int avg;
+    int avg;
     for (int i = 0; i < height; i++)
     {
 
         for (int j = 0; j < width; j++)
         {
         avg = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
-        avg = avg / 3;
-        float r = avg % 1;
-        float sub = 1 - r;
-        if (r > 0.0)
+        float r = avg % 3;
+        float sub = 1.0-r;
+        if (r > 0)
         {
-
             if (r >= 0.5)
             {
                 avg += sub;
@@ -24,7 +22,10 @@ int avg;
             {
                 avg -= r;
             }
+
         }
+
+        avg = avg / 3;
         image[i][j].rgbtRed = avg;
         image[i][j].rgbtGreen = avg;
         image[i][j].rgbtBlue = avg;
