5878cf779969340656344ca33d54718e6822cb30
siddven <siddven@users.noreply.github.com>
2021-02-28 19:39:36 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -17,16 +17,24 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         }
 
     }
-
-    
-
-
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width/2; j++)
+        {
+            RGBTRIPLE buffer = image[i][j];
+            image[i][j] = image[i][width-j-1];
+            image[i][width-j-1] = buffer;
+
+        }
+    }
+
     return;
 }
 
