f3ce05ded53c551008503d41f7f695c4f0912b05
siddven <siddven@users.noreply.github.com>
2021-03-14 22:16:40 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -320,14 +320,14 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
 
         //Calculate Gy for all color channels
-        avgblueY = buffer[i][j].rgbtBlue*0 + buffer[i+1][j].rgbtBlue*2 + buffer[i+1][j+1].rgbtBlue*-1 + buffer[i][j+1].rgbtBlue*0 + buffer[i-1][j].rgbtBlue*-2+
-        buffer[i-1][j-1].rgbtBlue*-1 + buffer[i][j-1].rgbtBlue*0 + buffer[i-1][j+1].rgbtBlue*1 + buffer[i+1][j-1].rgbtBlue*1;
+        avgblueY = buffer[i][j].rgbtBlue*0 + buffer[i+1][j].rgbtBlue*2 + buffer[i+1][j+1].rgbtBlue*1 + buffer[i][j+1].rgbtBlue*0 + buffer[i-1][j].rgbtBlue*-2+
+        buffer[i-1][j-1].rgbtBlue*-1 + buffer[i][j-1].rgbtBlue*0 + buffer[i-1][j+1].rgbtBlue*-1 + buffer[i+1][j-1].rgbtBlue*1;
 
-        avgredY = buffer[i][j].rgbtRed*0 + buffer[i+1][j].rgbtRed*2 + buffer[i+1][j+1].rgbtRed*-1 + buffer[i][j+1].rgbtRed*0 + buffer[i-1][j].rgbtRed*-2+
-        buffer[i-1][j-1].rgbtRed*-1 + buffer[i][j-1].rgbtRed*0 + buffer[i-1][j+1].rgbtRed*1 + buffer[i+1][j-1].rgbtRed*1;
+        avgredY = buffer[i][j].rgbtRed*0 + buffer[i+1][j].rgbtRed*2 + buffer[i+1][j+1].rgbtRed*1 + buffer[i][j+1].rgbtRed*0 + buffer[i-1][j].rgbtRed*-2+
+        buffer[i-1][j-1].rgbtRed*-1 + buffer[i][j-1].rgbtRed*0 + buffer[i-1][j+1].rgbtRed*-1 + buffer[i+1][j-1].rgbtRed*1;
 
-        avggreenY = buffer[i][j].rgbtGreen*0 + buffer[i+1][j].rgbtGreen*2 + buffer[i+1][j+1].rgbtGreen*-1 + buffer[i][j+1].rgbtGreen*0 + buffer[i-1][j].rgbtGreen*-2+
-        buffer[i-1][j-1].rgbtGreen*-1 + buffer[i][j-1].rgbtGreen*0 + buffer[i-1][j+1].rgbtGreen*1 + buffer[i+1][j-1].rgbtGreen*1;
+        avggreenY = buffer[i][j].rgbtGreen*0 + buffer[i+1][j].rgbtGreen*2 + buffer[i+1][j+1].rgbtGreen*1 + buffer[i][j+1].rgbtGreen*0 + buffer[i-1][j].rgbtGreen*-2+
+        buffer[i-1][j-1].rgbtGreen*-1 + buffer[i][j-1].rgbtGreen*0 + buffer[i-1][j+1].rgbtGreen*-1 + buffer[i+1][j-1].rgbtGreen*1;
 
         //Calculate square root of Gx and Gy
         avgblueX = sobel(avgblueX, avgblueY);
