ca4cf1ca5ed37f4f0431a31160cb8c29969d7d11
siddven <siddven@users.noreply.github.com>
2021-03-28 19:52:37 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -4,60 +4,60 @@
 #include <stdbool.h>
 int main(int argc, char *argv[])
 {
- FILE *placeholder = NULL;
- char filename[7];
- if (argc != 2)
- {
-     //checks if proper # of arguments are entered (1 argument)
-     printf("Usage: ./recover image\n");
-     return 1;
+    FILE *placeholder = NULL;
+    char filename[7];
+    if (argc != 2)
+    {
+        //checks if proper # of arguments are entered (1 argument)
+        printf("Usage: ./recover image\n");
+        return 1;
 
- }
- //opening the memory card
- FILE *file = fopen(argv[1], "r");
+    }
+//opening the memory card
+    FILE *file = fopen(argv[1], "r");
 
- uint8_t buffer[512];
- int check = 0;
- bool newjpeg = false;
- bool currentjpeg = false;
+    uint8_t buffer[512];
+    int check = 0;
+    bool newjpeg = false;
+    bool currentjpeg = false;
 
- while (fread(&buffer,sizeof(uint8_t),512,file) == 512)
- {
+    while (fread(&buffer, sizeof(uint8_t), 512, file) == 512)
+    {
 
-  if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0))
-  {
+        if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0))
+        {
 
-    newjpeg = true;
-    currentjpeg = true;
-  }
-  else
-  {
-   newjpeg = false;
-  }
-  if (newjpeg && currentjpeg)
-  {
-  if (placeholder != NULL)
-  {
-   fclose(placeholder);
-  }
-  sprintf(filename, "%03i.jpg", check);
-  check ++;
-  placeholder = fopen(filename,"w");
-  fwrite(&buffer, sizeof(uint8_t), 512, placeholder);
-  newjpeg = false;
-  }
+            newjpeg = true;
+            currentjpeg = true;
+        }
+        else
+        {
+            newjpeg = false;
+        }
+        if (newjpeg && currentjpeg)
+        {
+            if (placeholder != NULL)
+            {
+                fclose(placeholder);
+            }
+            sprintf(filename, "%03i.jpg", check);
+            check ++;
+            placeholder = fopen(filename, "w");
+            fwrite(&buffer, sizeof(uint8_t), 512, placeholder);
+            newjpeg = false;
+        }
 
-  else if (currentjpeg)
-  {
-   fwrite(&buffer, sizeof(uint8_t), 512, placeholder);
+        else if (currentjpeg)
+        {
+            fwrite(&buffer, sizeof(uint8_t), 512, placeholder);
 
-  }
+        }
 
-  else
-  {
-   newjpeg = false;
-  }
+        else
+        {
+            newjpeg = false;
+        }
 
- }
- fclose(placeholder);
+    }
+    fclose(placeholder);
 }
