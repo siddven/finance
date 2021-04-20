ade16e5162e4a457d15ae2fc28cfa3c1115908b0
siddven <siddven@users.noreply.github.com>
2021-04-20 20:34:18 +0000

    automated commit by check50 [check50=True]

--- 7.sql
+++ 7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM ratings JOIN movies ON ratings.movie_id = movies.id WHERE year = 2010 ORDER BY rating DESC;
+SELECT title, rating FROM ratings JOIN movies ON ratings.movie_id = movies.id WHERE year = 2010 ORDER BY 2 DESC, 1;
