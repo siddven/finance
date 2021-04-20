f68f56c428496d6208629e87aa020108214e74f5
siddven <siddven@users.noreply.github.com>
2021-04-20 20:27:34 +0000

    automated commit by check50 [check50=True]

--- 6.sql
+++ 6.sql
@@ -1,1 +1,1 @@
-SELECT AVG(rating) from ratings JOIN movies ON ratings.movie_id = movies.id WHERE year = 2012;
+SELECT AVG(rating) from ratings JOIN movies ON ratings.movie_id = movie.id WHERE year = 2012;
