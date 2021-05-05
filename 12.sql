5fbe0dd6ed9aa3eabdca971c18009bbd013a5976
siddven <siddven@users.noreply.github.com>
2021-05-05 15:47:09 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,3 +1,6 @@
-SELECT title
-FROM movies
-WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name == "Johnny Depp" OR name == "Helena Bonham Carter"));
+
+SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name == "Kevin Bacon" and birth == 1958);
+
+
+SELECT movie_id FROM stars WHERE person_id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name == "Kevin Bacon" and birth == 1958);
+)
