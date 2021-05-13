5ece13f0d2802d828ad5b378a95a064bb8b3d88f
siddven <siddven@users.noreply.github.com>
2021-05-13 00:06:01 +0000

    automated commit by check50 [check50=True]

--- 13.sql
+++ 13.sql
@@ -1,2 +1,5 @@
-SELECT people FROM SELECT title FROM movies WHERE id IN
-(SELECT movie_id FROM stars WHERE person_id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name == "Kevin Bacon" and birth == 1958)));
+SELECT name FROM people WHERE NOT name = "Kevin Bacon" and name IN
+(SELECT name FROM people WHERE id IN
+(SELECT person_id FROM stars WHERE movie_id IN
+(SELECT id FROM movies WHERE id IN
+(SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon")))));
